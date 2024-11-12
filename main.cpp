95fe1c9800e4bb7edc37edea71e994b5e456a3a7
Jadon Mensah <35032203+jadonmensah@users.noreply.github.com>
2024-11-12 23:52:37 +0000

    implemented run function (buggy)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-// AQA assembly interpreter - last updated 11/11/24 by Jadon Mensah
-// Notes: implemented bytecode function. TODO implement run function
+// AQA assembly interpreter - last updated 12/11/24 by Jadon Mensah
+// Notes: implemented run function. program compiles - now need to fix bugs!
 
 #include <bits/stdc++.h>
-#define SZ_INST_MEM sizeof(int[1024])
-#define SZ_DATA_MEM sizeof(int16_t[1024]) // max 1048576 addressable locations
-#define NUM_REGS 12 // maximum of 13 addressable locations
+#define SZ_INST_MEM 1024
+#define SZ_DATA_MEM 1024 // max 1048576 addressable locations
+#define NUM_REGS 13 // maximum of 13 addressable locations
 
 using namespace std;
 
@@ -28,7 +28,7 @@ enum opcodes
     LSR,
 };
 
-char mnemonic2opcode(string(s))
+char mnemonic2opcode(string s)
 {
     string s_lower;
     for (int i = 0; i < s.length(); i++)
@@ -55,6 +55,16 @@ char mnemonic2opcode(string(s))
         {"halt",  HALT},
     };
     return opcodes_local[s_lower];
+}
+
+string memdisplay(int16_t *m, unsigned int sz)
+{
+    string s;
+    for (int i = 0; i < sz; i++) {
+        s += to_string(m[i]);
+        s += " | ";
+    }
+    return s;
 }
 
 int bytecode(string line, map<string, int> label_table)
@@ -88,19 +98,25 @@ int bytecode(string line, map<string, int> label_table)
         line.erase(0, delimiter_position + delimiter.length());
         token_counter += 1;
     }
-    int operation;
+
+    // Assuming inputs are consistent with specification
+    int operation = mnemonic2opcode(instruction[0]) << 28;
+    int register1 = stoi(instruction[1].substr(1)) << 24;
+    int register2 = stoi(instruction[2].substr(1)) << 20;
+    int registerval1 = (stoi(instruction[2].substr(1)) | 0x00FFFFF0);
+    int registerval2 = (stoi(instruction[3].substr(1)) | 0x000FFFF0);
+    int immediate1 = stoi(instruction[2].substr(1));
+    int immediate2 = stoi(instruction[3].substr(1));
+    int memory_ref;
+    try {
+        memory_ref = stoi(instruction[2]) | 0x00AA0000;
+    } catch (...) {
+        memory_ref = 0x00AA0000;
+    }
+    int label = label_table[instruction[1]] | 0x01110000;
     switch (mnemonic2opcode(instruction[0]))
     {
-    default: // Assuming inputs are consistent with specification
-        operation = mnemonic2opcode(instruction[0]) << 28;
-        int register1 = stoi(instruction[1].substr(1)) << 24;
-        int register2 = stoi(instruction[2].substr(1)) << 20;
-        int registerval1 = (stoi(instruction[2].substr(1)) | 0x00FFFFF0);
-        int registerval2 = (stoi(instruction[3].substr(1)) | 0x000FFFF0);
-        int immediate1 = stoi(instruction[2].substr(1));
-        int immediate2 = stoi(instruction[3].substr(1));
-        int memory_ref = stoi(instruction[2]) | 0x00AA0000;
-        int label = label_table[instruction[1]] | 0x01110000;
+
     case LDR: case STR:
         operation |= register1 | memory_ref;
         break;
@@ -127,15 +143,88 @@ int bytecode(string line, map<string, int> label_table)
     return operation;
 }
 
+int run(int inst, int ip, int16_t *regs, int16_t *data_memory, char *flag)
+{
+    // modify regs/memory/flag as needed
+    // set ip increment
+    // select instruction by opcode & modify regs/memory/flag as necessary
+    int opcode = (inst >> 28) & 0xF;
+    int register1 = (inst >> 24) & 0xF;
+    int register2 = (inst >> 20) & 0xF;
+    int registerval = inst & 0xF;
+    int16_t immediate = inst & 0xFFFF;
+    int16_t memory_ref = inst & 0xFFFF;
+    int16_t jump_dest = inst & 0xFFFF;
+    int16_t operand2;
+    !((inst >> 16) & 0xF) ? operand2 = immediate : operand2 = regs[registerval];
+
+    switch(opcode)
+    {
+
+    case LDR:
+        regs[register1] = data_memory[memory_ref];
+        break;
+    case STR:
+        data_memory[memory_ref] = regs[register1];
+        break;
+    case ADD:
+        regs[register1] = regs[register2] + operand2;
+        break;
+    case SUB:
+        regs[register1] = regs[register2] - operand2;
+        break;
+    case MOV:
+        regs[register2] = operand2;
+        break;
+    case CMP:
+        *flag = (regs[register1] == operand2) & 0x1
+             | ((regs[register1] < operand2) << 1) & 0b10;
+        break;
+    case B:
+        return jump_dest - ip;
+        break;
+    case BEQ:
+        if (*flag & 0x1) return jump_dest - ip;
+        break;
+    case BLT:
+        if (*flag & 0b10) return jump_dest - ip;
+        break;
+    case AND:
+        regs[register1] == regs[register2] & operand2;
+        break;
+    case ORR:
+        regs[register1] == regs[register2] | operand2;
+        break;
+    case EOR:
+        regs[register1] == regs[register2] ^ operand2;
+        break;
+    case MVN:
+        regs[register1] == ~operand2;
+        break;
+    case LSL:
+        regs[register1] == regs[register2] << operand2;
+        break;
+    case LSR:
+        regs[register1] == regs[register2] >> operand2;
+        break;
+    case HALT:
+        // this case should not be reachable
+        cout << "[debug] HALT case reached in run()\n";
+
+    }
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
     // initialise various things we'll need later
-    int *instruction_memory = malloc(SZ_INST_MEM);
-    int16_t *data_memory = malloc(SZ_DATA_MEM);
-    memset(data_memory, 0, SZ_DATA_MEM);
+    int *instruction_memory = new int[SZ_INST_MEM];
+    int16_t *data_memory = new int16_t[SZ_DATA_MEM];
+    fill(data_memory, data_memory + SZ_DATA_MEM, 0);
     int16_t regs[NUM_REGS];
     map<string, int> label_table;
     int instruction_counter, length;
+    char flag = 0; // LSB - equal flag, 2nd least significant bit - LT flag
 
     // iterate through lines in file specified by first argument
     std::ifstream file(argv[1]);
@@ -157,11 +246,17 @@ int main(int argc, char *argv[])
     // once file has been parsed, execute from instruction_memory[0] until HALT
     for(int ip = 0; // instruction pointer
             instruction_memory[ip] != HALT;
-            ip += run(instruction_memory[ip], ip, regs, data_memory, &flag);)
+            ip += run(instruction_memory[ip], ip, regs, data_memory, &flag))
     {
-        // display both instruction and data memories, registers, flag and ip
+        // display data memory, registers, flag and ip
+        cout << "Registers: " << memdisplay(regs, NUM_REGS) << "\n";
+        cout << "Memory: "
+             << memdisplay(data_memory, SZ_DATA_MEM) << "\n";
+        cout << "Equality flag: " << (flag & 0x1) << "\n";
+        cout << "Less than flag: " << (flag & 0b10 >> 1) << "\n";
+
     }
-    free(instruction_memory);
-    free(data_memory);
+    delete[] instruction_memory;
+    delete[] data_memory;
     return 0;
 }
